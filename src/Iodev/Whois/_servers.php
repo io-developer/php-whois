8717bfa50f1a6e2f6ff56101c9754ef59dbdf2e8
io-developer <i.o.developer@gmail.com>
2017-10-15 01:40:34 +0300

    Refactor default servers

--- src/Iodev/Whois/_servers.php
+++ src/Iodev/Whois/_servers.php
@@ -1,26 +1,10 @@
 <?php
 
 return [
-    [
-        'zone' => '.com',
-        'host' => 'whois.crsnic.net',
-        'centralized' => false,
-        'parser' => '\Iodev\Whois\Parsers\ComParser',
-    ], [
-        'zone' => '.net',
-        'host' => 'whois.crsnic.net',
-        'centralized' => false,
-        'parser' => '\Iodev\Whois\Parsers\RuParser',
-    ], [
-        'zone' => '.ru',
-        'host' => 'whois.ripn.net',
-        'centralized' => true,
-        'parser' => '\Iodev\Whois\Parsers\RuParser',
-    ], [
-        // .рф
-        'zone' => '.xn--p1ai',
-        'host' => 'whois.ripn.net',
-        'centralized' => true,
-        'parser' => '\Iodev\Whois\Parsers\RuParser',
-    ],
+    [ 'zone' => '.com', 'host' => 'whois.crsnic.net' ],
+    [ 'zone' => '.net', 'host' => 'whois.crsnic.net' ],
+    [ 'zone' => '.ru', 'host' => 'whois.ripn.net', 'parser' => '\Iodev\Whois\Parsers\RuParser' ],
+
+    // .рф
+    [ 'zone' => '.xn--p1ai', 'host' => 'whois.ripn.net', 'parser' => '\Iodev\Whois\Parsers\RuParser' ],
 ];
