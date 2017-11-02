cce7b8d0bb5c522deb5b8b2a90815218227c4e78
io-developer <i.o.developer@gmail.com>
2017-11-03 00:53:16 +0300

    Add ResponseTest

---
+++ tests/Iodev/Whois/ResponseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Iodev\Whois;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Response */
+    private $resp;
+
+    public function setUp()
+    {
+        $this->resp = new Response("domain.some", "Test content", "whois.host.abc");
+    }
+
+
+    public function testGetDomain()
+    {
+        self::assertEquals("domain.some", $this->resp->getDomain());
+    }
+
+    public function testGetText()
+    {
+        self::assertEquals("Test content", $this->resp->getText());
+    }
+
+    public function testGetWhoisHost()
+    {
+        self::assertEquals("whois.host.abc", $this->resp->getWhoisHost());
+    }
+}
