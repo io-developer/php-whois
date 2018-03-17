6a0a054b182c2ab1bc15c0ae83246033c763355d
io-developer <i.o.developer@gmail.com>
2018-03-18 00:14:59 +0300

    feat (parsing): add group parsing in BlockParser

--- src/Iodev/Whois/Parsers/BlockParser.php
+++ src/Iodev/Whois/Parsers/BlockParser.php
@@ -8,5 +8,45 @@ use Iodev\Whois\Helpers\GroupHelper;
 
 class BlockParser extends CommonParser
 {
-   
+    const HEADER_KEY = ' header ';
+
+    /**
+     * @param Response $response
+     * @return DomainInfo
+     */
+    public function parseResponse(Response $response)
+    {
+        return null;
+        $groups = $this->groupsFromText($response->getText());
+        var_dump($groups);
+        return null;
+    }
+
+    /**
+     * @param string $text
+     * @return array
+     */
+    protected function groupFromText($text)
+    {
+        $group = [];
+        $hasHeader = false;
+        foreach (preg_split('~\r\n|[\r\n]~u', $text) as $line) {
+            if ($hasHeader && ltrim($line, '%#*') !== $line) {
+                continue;
+            }
+            $split = explode(':', ltrim($line, "%#*:;= \t\0\x0B"), 2);
+            $k = isset($split[0]) ? trim($split[0]) : '';
+            $v = isset($split[1]) ? trim($split[1]) : '';
+            if (strlen($k) && strlen($v)) {
+                $group = array_merge_recursive($group, [ $k => $v ]);
+                continue;
+            }
+            $k = trim($k, "%#*:;=[] \t\0\x0B");
+            if (strlen($k) && !$hasHeader) {
+                $group = array_merge_recursive($group, [ self::HEADER_KEY => $k ]);
+                $hasHeader = true;
+            }
+        }
+        return $group;
+    }
 }
