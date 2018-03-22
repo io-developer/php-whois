d8528d98b56c7ff9be10478efd0d189658b2625c
io-developer <i.o.developer@gmail.com>
2018-03-22 22:50:28 +0300

    fix (domains): remove mb_strtolower() to save unicode on PHP 5.5-

--- src/Iodev/Whois/Helpers/DomainHelper.php
+++ src/Iodev/Whois/Helpers/DomainHelper.php
@@ -56,13 +56,13 @@ class DomainHelper
      * @param string $domain
      * @return string
      */
-    public static function correct($domain)
+    private static function correct($domain)
     {
         // Fix for .UZ whois response
         while (preg_match('~\bnot\.defined\.?\b~ui', $domain)) {
             $domain = preg_replace('~\bnot\.defined\.?\b~ui', '', $domain);
         }
-        return mb_strtolower(rtrim(preg_replace('~\s*\.\s*~ui', '.', $domain), ".-\t "));
+        return rtrim(preg_replace('~\s*\.\s*~ui', '.', $domain), ".-\t ");
     }
 
     /**
