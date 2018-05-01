f832370890a5c7f6201e5fe6c25383fb14c3e503
io-developer <i.o.developer@gmail.com>
2018-05-01 15:51:44 +0300

    refact (response): remove unused code

--- src/Iodev/Whois/Response.php
+++ src/Iodev/Whois/Response.php
@@ -2,21 +2,8 @@
 
 namespace Iodev\Whois;
 
-use Iodev\Whois\Modules\ModuleType;
-
 class Response
 {
-    /**
-     * @param string $domain
-     * @param string $text
-     * @param string $whoisHost
-     * @return Response
-     */
-    public static function createDomainResponse($domain, $text, $whoisHost = "")
-    {
-        return new Response(ModuleType::TLD, $domain, $text, $whoisHost);
-    }
-
     public function __construct($query = "", $text = "", $host = "")
     {
         $this->query = strval($query);
