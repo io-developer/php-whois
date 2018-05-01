35877f5290733c7f8e94d3e790c0bc89fe5f6118
io-developer <i.o.developer@gmail.com>
2018-05-01 15:57:35 +0300

    feat (asn): implement data objects

--- src/Iodev/Whois/Modules/Asn/AsnInfo.php
+++ src/Iodev/Whois/Modules/Asn/AsnInfo.php
@@ -2,7 +2,58 @@
 
 namespace Iodev\Whois\Modules\Asn;
 
+use InvalidArgumentException;
+
+/**
+ * Immutable data object
+ */
 class AsnInfo
 {
+    /**
+     * @param AsnResponse $response
+     * @param string $asn
+     * @param AsnInfoRoute[] $routes
+     */
+    public function __construct(AsnResponse $response, $asn, $routes)
+    {
+        if (!is_array($routes)) {
+            throw new InvalidArgumentException("Routes must be an array");
+        }
+        $this->response = $response;
+        $this->asn = strval($asn);
+        $this->routes = $routes;
+    }
 
+    /** @var AsnResponse */
+    private $response;
+
+    /** @var string */
+    private $asn;
+
+    /** @var AsnInfoRoute[] */
+    private $routes;
+
+    /**
+     * @return AsnResponse
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAsn()
+    {
+        return $this->asn;
+    }
+
+    /**
+     * @return AsnInfoRoute[]
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 }
