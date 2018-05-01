df4fa2cb9da3c2af427bbccb855dc07e6f6d2fe1
io-developer <i.o.developer@gmail.com>
2018-05-01 16:22:33 +0300

    feat (asn): implement AsnParser

--- src/Iodev/Whois/Modules/Asn/AsnParser.php
+++ src/Iodev/Whois/Modules/Asn/AsnParser.php
@@ -4,5 +4,56 @@ namespace Iodev\Whois\Modules\Asn;
 
 class AsnParser
 {
+    /** @var string[]  */
+    private $routeKeys = [
+        'route',
+        'route6',
+        'descr',
+        'origin',
+        'mnt-by',
+        'changed',
+        'source',
+    ];
 
+    /**
+     * @param AsnResponse $response
+     * @return AsnInfo
+     */
+    public function parseResponse(AsnResponse $response)
+    {
+        return new AsnInfo($response, $response->getAsn(), $this->parseRoutes($response));
+    }
+
+    /**
+     * @param AsnResponse $asnResponse
+     * @return AsnInfoRoute[]
+     */
+    private function parseRoutes(AsnResponse $asnResponse)
+    {
+        $routes = [];
+        $separator = "\r\n";
+        $line = strtok($asnResponse->getText(), $separator);
+        $data = null;
+        while ($line !== false) {
+            foreach ($this->routeKeys as $key) {
+                if (strpos($line, "$key:") === 0) {
+                    if ($key === 'route' || $key == 'route6') {
+                        if (!empty($data)) {
+                            $routes[] = new AsnInfoRoute($data);
+                        }
+                        $data = [];
+                    }
+                    if (isset($data)) {
+                        $data[$key] = trim(str_replace("$key:", '', $line));
+                    }
+                    break;
+                }
+            }
+            $line = strtok($separator);
+        }
+        if (!empty($data)) {
+            $routes[] = new AsnInfoRoute($data);
+        }
+        return $routes;
+    }
 }
