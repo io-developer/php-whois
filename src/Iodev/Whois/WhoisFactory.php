b187cfb3197f3bf47d0008858a391b2c3b41f284
io-developer <i.o.developer@gmail.com>
2019-08-19 14:11:52 +0300

    refact (instantiating): replace some code by WhoisFactory

--- src/Iodev/Whois/WhoisFactory.php
+++ src/Iodev/Whois/WhoisFactory.php
@@ -7,16 +7,30 @@ use Iodev\Whois\Loaders\SocketLoader;
 use Iodev\Whois\Modules\Asn\AsnModule;
 use Iodev\Whois\Modules\Asn\AsnServer;
 use Iodev\Whois\Modules\Tld\TldModule;
+use Iodev\Whois\Modules\Tld\TldParser;
 use Iodev\Whois\Modules\Tld\TldServer;
 
 class WhoisFactory implements IWhoisFactory
 {
     /**
+     * @return WhoisFactory
+     */
+    public static function getInstance(): WhoisFactory
+    {
+        static $instance;
+        if (!$instance) {
+            $instance = new static();
+        }
+        return $instance;
+    }
+
+    /**
+     * @param ILoader|null $loader
      * @return Whois
      */
-    public function createWhois(): Whois
+    public function createWhois(ILoader $loader = null): Whois
     {
-        $whois = new Whois($this->createLoader());
+        $whois = new Whois($loader ?: $this->createLoader());
         $whois->setFactory($this);
         return $whois;
     }
@@ -49,7 +63,53 @@ class WhoisFactory implements IWhoisFactory
     public function createTldModule(ILoader $loader = null, $servers = null): TldModule
     {
         $m = new TldModule($loader);
-        $m->setServers($servers ?: TldServer::fromDataList(Config::load("module.tld.servers")));
+        $m->setServers($servers ?: $this->createTldSevers());
         return $m;
+    }
+
+    /**
+     * @return TldServer[]
+     */
+    public function createTldSevers(): array
+    {
+        $servers = [];
+        foreach (Config::load("module.tld.servers") as $config) {
+            $servers[] = $this->createTldSever($config);
+        }
+        return $servers;
+    }
+
+    /**
+     * @param array $config
+     * @return TldServer
+     */
+    public function createTldSever(array $config): TldServer
+    {
+        return new TldServer(
+            $config['zone'] ?? '',
+            $config['host'] ?? '',
+            !empty($config['centralized']),
+            $this->createTldSeverParser($config),
+            $config['queryFormat'] ?? null
+        );
+    }
+
+    /**
+     * @param array $config
+     * @return TldParser
+     */
+    public function createTldSeverParser(array $config): TldParser
+    {
+        $options = $config['parserOptions'] ?? [];
+        if (isset($config['parserClass'])) {
+            return TldParser::createByClass(
+                $config['parserClass'],
+                $config['parserType'] ?? null
+            )->setOptions($options);
+        }
+        if (isset($config['parserType'])) {
+            return TldParser::create($config['parserType'])->setOptions($options);
+        }
+        return TldParser::create()->setOptions($options);
     }
 }
