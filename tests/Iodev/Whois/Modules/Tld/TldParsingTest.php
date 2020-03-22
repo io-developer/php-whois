7ef6357fb6b26fb945705f3b35d79b5e96064782
Riccardo Bessone <riccardo@bess.one>
2020-03-22 22:20:00 +0100

    feat (tld/servers): add .cam (#104)

--- tests/Iodev/Whois/Modules/Tld/TldParsingTest.php
+++ tests/Iodev/Whois/Modules/Tld/TldParsingTest.php
@@ -244,6 +244,10 @@ class TldParsingTest extends TestCase
             [ "free.bz", ".bz/free.txt", null ],
             [ "google.com.bz", ".bz/google.com.bz.txt", ".bz/google.com.bz.json" ],
 
+            // .CAM
+            [ "free.cam", ".cam/free.txt", null ],
+            [ "google.cam", ".cam/google.cam.txt", ".cam/google.cam.json" ],
+
             // .CAT
             [ "free.cat", ".cat/free.txt", null ],
             [ "google.cat", ".cat/google.cat.txt", ".cat/google.cat.json" ],
