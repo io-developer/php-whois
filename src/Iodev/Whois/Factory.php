7d14cca6e11c9b939edbd6cf771e41a515ea3d0d
Zero Huang <john.royer@gmail.com>
2023-01-12 16:59:55 +0800

    Using symfony/polyfill-intl-idn instead of true/php-punycode (#195)
    * Composer install symfony/polyfill-intl-idn
    
    * Rewrite convertor
    
    use Symfony\Polyfill\Intl instead of Punycode
    
    * Rename class
    
    not TruePunycode anymore
    
    * Uninstall true/punycode from composer
    
    * Use Symfony Idn instead of TruePunycode
    
    * Rename interface
    
    * compose update before unit test
    
    Different PHP version has different dependency. And
    file `composer.lock` has been ignored in `.gitignore`.
    
    Use `composer update` instead of `composer install`.
    
    * Revert "compose update before unit test"
    
    This reverts commit 0f3d04e8fd837cf38ffa3a4f6db8474462a0090f.
    
    `composer install` is also works. Keep current script un-changed.
    
    * Revert "Rename interface"
    
    This reverts commit 76dd0563f65789e9e6084f893f6d5b44aafda3af.
    
    Do not fix PSR issue at this PR.
    
    * Remove `idn_to_*()` checker
    
    * Remove class SymfonyPunycodeCovertor

--- src/Iodev/Whois/Factory.php
+++ src/Iodev/Whois/Factory.php
@@ -16,7 +16,6 @@ use Iodev\Whois\Modules\Tld\TldParser;
 use Iodev\Whois\Modules\Tld\TldServer;
 use Iodev\Whois\Punycode\IntlPunycode;
 use Iodev\Whois\Punycode\IPunycode;
-use Iodev\Whois\Punycode\TruePunycode;
 
 class Factory implements IFactory
 {
@@ -34,10 +33,7 @@ class Factory implements IFactory
 
     public function createPunycode(): IPunycode
     {
-        if (function_exists("idn_to_utf8") && function_exists("idn_to_ascii")) {
             return new IntlPunycode();
-        }
-        return new TruePunycode();
     }
 
     /**
